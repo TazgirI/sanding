f2ce1a508498e9571710a6de955aa87c630a6d78
TM316157 <tallenmcgill@gmail.com>
2025-04-20 07:28:54 +0100

    Minor setup

---
+++ src/main/java/net/tazgirl/sanding/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.tazgirl.sanding.item;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.tazgirl.sanding.Sanding;
+
+public class ModItems
+{
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Sanding.MODID);
+
+}
